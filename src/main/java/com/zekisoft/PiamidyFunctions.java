be1e39fa8bf1ce961882873836da785dd548c824
hatoimo1 <hatoimo1@gmail.com>
2023-03-06 22:51:19 +0900

    初期コミット

---
+++ src/main/java/com/zekisoft/PiamidyFunctions.java
@@ -0,0 +1,34 @@
+package com.zekisoft;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class PiamidyFunctions {
+    public static void main(String... args) {
+        SpringApplication.run(PiamidyFunctions.class, args);
+    }
+
+    @Bean
+    public Supplier<Mono<String>> hello() {
+        return () -> Mono.just("Hello Spring PiamidyFunctions!!");
+    }
+	
+    @Bean
+    public Function<Mono<String>, Mono<String>> decorate() {
+        return message -> message.map(m -> String.format("★★★%s★★★", m));
+    }
+
+    @Bean
+    public Consumer<Flux<String>> consume() {
+        return message -> message.subscribe(m -> System.out.printf("Received: %s%n", m));
+    }
+}
